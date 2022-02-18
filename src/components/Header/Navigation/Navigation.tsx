6a2882b0edba5b02d3c20f9ed4e0c4deb39c50e0
Daniil <portal.sid.grand2@gmail.com>
2022-02-18 09:29:50 +0200

    Logo component remove

--- src/components/Header/Navigation/Navigation.tsx
+++ src/components/Header/Navigation/Navigation.tsx
@@ -1,27 +1,13 @@
 import { FC } from "react";
-import { Menu } from "antd";
-import { Logo } from "../Logo/Logo";
+import { Menu, Image } from "antd";
+import logo from '../../../img/logo.png';
 
 
 
 export const Navigation: FC = () => {
-  const menuItem = {
-    display: 'flex',
-    alignItems: 'flex-end',
-  }
+
+  
   return (
-    <Menu theme="light" mode="horizontal" 
-    defaultSelectedKeys={["2"]}>
-      <Menu.Item key="logo">
-        <Logo />
-      </Menu.Item>
-      <Menu.Item key="0" style={menuItem}>Main</Menu.Item>
-      <Menu.Item key="1" style={menuItem}>Organizations</Menu.Item>
-      <Menu.Item key="2" style={menuItem}>Individuals</Menu.Item>
-      <Menu.Item key="3" style={menuItem}>Tech</Menu.Item>
-      <Menu.Item key="4" style={menuItem}>Team</Menu.Item>
-      <Menu.Item key="5" style={menuItem}>News</Menu.Item>
-      <Menu.Item key="6" style={menuItem}>Contact</Menu.Item>
-    </Menu>
+    <Image src={logo} preview={false} />
   );
 };
