4725777c9af4710b8a99349287fdaf61b99ad9ee
Daniil <portal.sid.grand2@gmail.com>
2022-02-18 09:42:24 +0200

    Rewrote App from inline styles to styled-components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,21 +4,32 @@ import { Navigation } from "../Header/Navigation/Navigation";
 import { FrontPage } from "../FrontPage/FrontPage";
 import { HowItWorks } from "../HowItWorks/HowItWorks";
 import { AboutTula } from "../AboutTula/AboutTula";
+import styled from 'styled-components';
 
 const { Header, Footer, Content } = Layout;
 
+const ModifiedLayout = styled(Layout)`
+  background: #ffffff;
+`;
+const ModifiedHeader = styled(Header)`
+  background: #ffffff;
+  padding: 10px;
+  height: auto;
+`;
+
+
 export const App: FC = () => {
   return (
-    <Layout>
-      <Header style={{ background: "white", height: "auto" }}>
+    <ModifiedLayout>
+      <ModifiedHeader>
         <Navigation />
-      </Header>
-      <Content style={{ background: "white" }}>
+      </ModifiedHeader>
+      <Content>
         <FrontPage />
         <HowItWorks />
         <AboutTula />
       </Content>
-      <Footer style={{ background: "white" }}>Footer</Footer>
-    </Layout>
+      <Footer>Footer</Footer>
+    </ModifiedLayout>
   );
 };
--- src/components/Header/Navigation/Navigation.tsx
+++ src/components/Header/Navigation/Navigation.tsx
@@ -1,13 +1,21 @@
 import { FC } from "react";
-import { Menu, Image } from "antd";
+import { Image, Row, Col } from "antd";
 import logo from '../../../img/logo.png';
+import styled from 'styled-components';
 
+const Wrapper = styled(Row)`
+  border: 1px solid black;
 
+`;
 
 export const Navigation: FC = () => {
 
-  
+
   return (
-    <Image src={logo} preview={false} />
+    <Wrapper>
+      <Image src={logo} preview={false} />
+
+    </Wrapper>
+
   );
 };
